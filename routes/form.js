8517355d57d6dd6a896104173f37e34c7f6adb5a
ZihaoChen-cogsci <z5chen@ucsd.edu>
2021-02-25 09:50:26 +0800

    Update form.js

--- routes/form.js
+++ routes/form.js
@@ -1,7 +1,17 @@
 /*
  * GET form page.
  */
-
-exports.view = function(req, res){
-    res.render('form');
+var data = require('../data.json');
+/*var pic = driver.findElements(By.className('tempic'));*/
+/*exports.view = function(req, res){
+    for (var i = 0; i < data.length; i++) {
+      if (pic[i].style.display=='block') {
+        data.templates[i].selected=true;
+      }
+      console.log(data);
+      res.render('form',data);
+    } */
+    exports.view = function(req, res){
+    console.log(data);
+    res.render('form',data);
   };
