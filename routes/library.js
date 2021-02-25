18a5c8e4b15815f952ea356cebd7933ebbd7c674
ZihaoChen-cogsci <z5chen@ucsd.edu>
2021-02-25 09:50:34 +0800

    Update library.js

--- routes/library.js
+++ routes/library.js
@@ -1,4 +1,8 @@
 var data = require('../data.json');
 exports.view = function(req, res){
+    for (var i = 0; i < data.length; i++) {
+    data.templates[i].selected=false;
+  }
+    console.log(data);
     res.render('library',data);
   };
