388594bbfa853af7dc1d2ea2fac000ce8b65dd32
Wang, Danling <daw001@ucsd.edu>
2021-02-28 22:30:12 -0800

    complete add form fields

--- public/js/form.js
+++ public/js/form.js
@@ -1,22 +1,32 @@
 'use strict';
 
+let i = 0;
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
 })
 
 function initializePage() {
-	// $("h3 a").click(function(e) {
-	// 	e.preventDefault();
-	// 	const originalText = $(this).text();
-	// 	$(this).text(anagrammedName(originalText));
-	// });
-  // $("#dimension").change(function() {
-  //   console.log($("#dimension"));
-  // })
+	const formFields = $("#form").serializeArray();
+	console.log("formFields!!!", formFields);
+	$("#add-field-button").click(onAddField);
 }
 
-// function onDimensionChange() {
-//   const val = $("#dimension").value;
-//   console.log($("#dimension"));
-// }
+function onAddField() {
+	const fieldName = $("#new-field-label").val();
+	const fieldInput = $("#new-field-input").val();
+
+	$("#new-fields").append('<div id="new-field' + i + '" class="new-field-container"><div class="input-field" style="width: 38%;"><input id="new-field-label" class="input" type="text" name="new-field-label" placeholder="label (eg. color)"></div><div class="input-field" style="width: 52%;"><input id="new-field-input" class="input" type="text" name="new-field-input" placeholder="value (eg. brown)"></div><div style="width: 8%;" onclick="deleteField(' + i + ')">x</div></div>');
+
+	i++;
+
+	console.log({fieldName, fieldInput});
+}
+
+function deleteField(deleteId) {
+	console.log(deleteId);
+	const id = "#new-field" + deleteId;
+	
+	$(id).remove();
+}
